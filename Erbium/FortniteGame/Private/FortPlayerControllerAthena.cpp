3a214b9484fc0bc068c632d29d0e7cdababddcc3
Sarah <me@ploosh.dev>
2025-10-07 22:41:39 -0400

    ok

--- Erbium/FortniteGame/Private/FortPlayerControllerAthena.cpp
+++ Erbium/FortniteGame/Private/FortPlayerControllerAthena.cpp
@@ -1065,7 +1065,7 @@ void AFortPlayerControllerAthena::Hook()
 	auto ServerReturnToMainMenuIdx = GetDefaultObj()->GetFunction("ServerReturnToMainMenu")->GetVTableIndex();
 	Utils::Hook<AFortPlayerControllerAthena>(ServerReturnToMainMenuIdx, DefaultFortPC->Vft[ServerReturnToMainMenuIdx]);
 
-	if (VersionInfo.EngineVersion >= 4.19)
+	if (VersionInfo.FortniteVersion != 1.72 && VersionInfo.FortniteVersion != 1.8)
 	{
 		Utils::ExecHook(GetDefaultObj()->GetFunction("ServerCreateBuildingActor"), ServerCreateBuildingActor);
 		Utils::ExecHook(GetDefaultObj()->GetFunction("ServerBeginEditingBuildingActor"), ServerBeginEditingBuildingActor);
