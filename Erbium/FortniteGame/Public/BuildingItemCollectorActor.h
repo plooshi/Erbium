ce1f08065de1d59c445beed258c668265813bff4
Sarah <me@ploosh.dev>
2025-10-10 17:43:28 -0400

    why...

--- Erbium/FortniteGame/Public/BuildingItemCollectorActor.h
+++ Erbium/FortniteGame/Public/BuildingItemCollectorActor.h
@@ -5,7 +5,43 @@
 struct FCollectorUnitInfo
 {
 public:
-    USCRIPTSTRUCT_COMMON_MEMBERS(FCollectorUnitInfo);
+    static const SDK::UStruct* StaticStruct()                         
+    {    
+        static const SDK::UStruct* _storage = nullptr;
+
+        if (!_storage)
+            _storage = SDK::FindStruct("CollectorUnitInfo");
+
+        if (!_storage)
+            _storage = SDK::FindStruct("ColletorUnitInfo");
+
+        return _storage;                                              
+    }    
+    static const int32 Size()                                         
+    {    
+        static int32 _size = -1;
+
+        if (_size == -1)                                              
+            _size = StaticStruct()->GetPropertiesSize();
+
+        return _size;
+    }    
+        
+    FCollectorUnitInfo& operator=(FCollectorUnitInfo& _Rhs)                                 
+    {    
+        __movsb((PBYTE)this, (const PBYTE)&_Rhs, Size());             
+        return *this;                                                 
+    }    
+
+    DEFINE_STRUCT_PROP(OutputItemEntry, TArray<FFortItemEntry>);
+    DEFINE_STRUCT_PROP(OutputItem, const UFortItemDefinition*);
+    DEFINE_STRUCT_PROP(InputCount, FScalableFloat);
+};
+
+struct FColletorUnitInfo
+{
+public:
+    USCRIPTSTRUCT_COMMON_MEMBERS(FColletorUnitInfo);
 
     DEFINE_STRUCT_PROP(OutputItemEntry, TArray<FFortItemEntry>);
     DEFINE_STRUCT_PROP(OutputItem, const UFortItemDefinition*);
