1e70867361733ebf7369a1289c6b2cb06161add4
kaitlyn <contact@mapleai.de>
2025-10-27 20:25:35 +0000

    john cena update

--- Erbium/Erbium/Plugins/CrashReporter/Public/CrashReporter.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "../../../../pch.h"
-
-class FCrashReporter
-{
-public:
-    static void Register();
-};
--- Erbium/SDK/Includes.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include "Offsets.h"
-#include "Containers.h"
-#include "Core.h"
-#include "Defs.h"
-#include "Engine.h"
