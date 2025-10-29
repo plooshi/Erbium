74f24f4f1228c414f554c2a7248dbde89a9c4400
sarah <me@ploosh.dev>
2025-10-29 01:54:57 -0400

    Enable the Graphical User Interface again.
    You see, this is a very important commit. In the last commit, I turned the GUI off. But, when i went to push it, I forgot to turn it back on. So, people who don't know anything about programming will have a hard time using it. Re-enabling the GUI fixes this issue, because then there will be a user friendly interface by default.

--- Erbium/Erbium/Public/Configuration.h
+++ Erbium/Erbium/Public/Configuration.h
@@ -11,8 +11,8 @@ struct FConfiguration
     static inline auto bInfiniteAmmo = false;
     static inline auto bForceRespawns = false;
     static inline auto bAutoRestart = false;
-    static inline constexpr auto bEnableIris = true;
+    static inline constexpr auto bEnableIris = true; 
     static inline constexpr auto bCreative = false;
-    static inline constexpr auto bGUI = false;
+    static inline constexpr auto bGUI = true;
     static inline constexpr auto bCustomCrashReporter = true;
 };
