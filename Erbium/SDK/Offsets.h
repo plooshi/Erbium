5b19e84f188b978e646c46916d76b5b0138078f5
Sarah <me@ploosh.dev>
2025-11-29 18:10:50 -0500

    fix 18.00 staticfindobject

--- Erbium/SDK/Offsets.h
+++ Erbium/SDK/Offsets.h
@@ -295,7 +295,8 @@ namespace SDK
 				Offsets::StaticFindObject = Memcury::Scanner::FindPattern("48 89 5C 24 ? 48 89 74 24 ? 48 89 7C 24 ? 55 41 54 41 55 41 56 41 57 48 8B EC 48 83 EC 60 45 33 ED 45 8A F9 44 38 2D ? ? ? ? 49 8B F8 48 8B").Get();
 			else if (VersionInfo.FortniteVersion == 16.50)
 				Offsets::StaticFindObject = Memcury::Scanner::FindPattern("48 89 5C 24 ? 48 89 74 24 ? 48 89 7C 24 ? 55 41 54 41 55 41 56 41 57 48 8B EC 48 83 EC 60 45 33 ED 45 8A F9 44 38 2D ? ? ? ? 49 8B F8 48 8B F2 4C 8B E1").Get();
-			else
+			
+			if (!Offsets::StaticFindObject)
 				Offsets::StaticFindObject = Memcury::Scanner::FindPattern("40 55 53 57 41 54 41 55 41 57 48 8D AC 24 ? ? ? ? 48 81 EC ? ? ? ? 48 8B 05 ? ? ? ? 48 33 C4 48 89 85").Get();
 		}
 		else if (VersionInfo.EngineVersion == 4.16)
