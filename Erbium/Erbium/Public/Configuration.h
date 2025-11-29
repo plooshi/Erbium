1383cb892987f9dc6a5ebd93b109bd83222eaeac
Sarah <me@ploosh.dev>
2025-11-29 18:34:07 -0500

    lol

--- Erbium/Erbium/Public/Configuration.h
+++ Erbium/Erbium/Public/Configuration.h
@@ -2,20 +2,20 @@
 
 struct FConfiguration
 {
-    static inline auto Playlist = L"/Game/Athena/Playlists/Showdown/Playlist_ShowdownAlt_Solo.Playlist_ShowdownAlt_Solo";
-    static inline auto MaxTickRate = 120.f;
-    static inline auto bLateGame = true;
-    static inline auto LateGameZone = 5; // starting zone
-    static inline auto bLateGameLongZone = true; // zone doesnt close for a long time
+    static inline auto Playlist = L"/Game/Athena/Playlists/Playlist_DefaultSolo.Playlist_DefaultSolo";
+    static inline auto MaxTickRate = 30.f;
+    static inline auto bLateGame = false;
+    static inline auto LateGameZone = 3; // starting zone
+    static inline auto bLateGameLongZone = false; // zone doesnt close for a long time
     static inline auto bEnableCheats = true;
     static inline auto SiphonAmount = 50; // set to 0 to disable
-    static inline auto bInfiniteMats = true;
-    static inline auto bInfiniteAmmo = true;
-    static inline auto bForceRespawns = true; // build your client with this too!
+    static inline auto bInfiniteMats = false;
+    static inline auto bInfiniteAmmo = false;
+    static inline auto bForceRespawns = false; // build your client with this too!
     static inline auto bJoinInProgress = false;
     static inline auto bAutoRestart = false;
     static inline auto bKeepInventory = false;
-    static inline constexpr auto bEnableIris = true;
+    static inline constexpr auto bEnableIris = true; 
     static inline constexpr auto bGUI = true;
     static inline constexpr auto bCustomCrashReporter = true;
     static inline constexpr auto bUseStdoutLog = false;
