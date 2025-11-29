0b65ba4b50f9d938c31e4e44b65f9ac7df82b287
Sarah <me@ploosh.dev>
2025-11-28 21:55:42 -0500

    uh

--- Erbium/FortniteGame/Private/FortInventory.cpp
+++ Erbium/FortniteGame/Private/FortInventory.cpp
@@ -91,8 +91,8 @@ UFortWorldItem* AFortInventory::GiveItem(const UFortItemDefinition* Def, int Cou
 
         if (Def->ItemType == EFortItemType::GetWeaponHarvest())
         {
-            PlayerController->ServerExecuteInventoryItem(Item->ItemEntry.ItemGuid);
-            PlayerController->QuickBars->ServerActivateSlotInternal(0, 0, 0.f, true);
+            //PlayerController->ServerExecuteInventoryItem(Item->ItemEntry.ItemGuid);
+            //PlayerController->QuickBars->ServerActivateSlotInternal(0, 0, 0.f, true);
         }
     }
 
@@ -287,8 +287,8 @@ void AFortInventory::Remove(FGuid Guid)
 
             if (Entry.ItemDefinition->ItemType == EFortItemType::GetWeaponHarvest())
             {
-                PlayerController->ServerExecuteInventoryItem(Entry.ItemGuid);
-                PlayerController->QuickBars->ServerActivateSlotInternal(0, 0, 0.f, true);
+                //PlayerController->ServerExecuteInventoryItem(Entry.ItemGuid);
+                //PlayerController->QuickBars->ServerActivateSlotInternal(0, 0, 0.f, true);
             }
         }
     }
