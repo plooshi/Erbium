8cae8e2b5a04af18680d279297e35a3f74b4e1c8
Sarah <me@ploosh.dev>
2025-12-26 22:51:10 -0500

    ill js do both of these ig

--- Erbium/Erbium/Private/Finders.cpp
+++ Erbium/Erbium/Private/Finders.cpp
@@ -3043,16 +3043,14 @@ void FindNullsAndRetTrues()
 
         if (RequestExit)
             NullFuncs.push_back(RequestExit);
-        else
-        {
-            auto RequestExitWithStatus = Memcury::Scanner::FindPattern("48 89 5C 24 ? 57 48 83 EC 40 41 B9 ? ? ? ? 0F B6 F9 44 38 0D ? ? ? ? 0F B6 DA 72 24 89 5C 24 30 48 8D 05 ? ? ? ? 89 7C 24 28 4C 8D 05 ? ? ? ? 33 D2 48 89 44 24 ? 33 C9 E8 ? ? ? ?").Get();
-            if (!RequestExitWithStatus)
-                RequestExitWithStatus = Memcury::Scanner::FindPattern("4C 8B DC 49 89 5B 08 49 89 6B 10 49 89 73 18 49 89 7B 20 41 56 48 83 EC 30 80 3D ? ? ? ? ? 49 8B").Get();
-            if (!RequestExitWithStatus)
-                RequestExitWithStatus = Memcury::Scanner::FindPattern("48 8B C4 48 89 58 18 88 50 10 88 48 08 57 48 83 EC 30").Get();
 
-            NullFuncs.push_back(RequestExitWithStatus);
-        }
+        auto RequestExitWithStatus = Memcury::Scanner::FindPattern("48 89 5C 24 ? 57 48 83 EC 40 41 B9 ? ? ? ? 0F B6 F9 44 38 0D ? ? ? ? 0F B6 DA 72 24 89 5C 24 30 48 8D 05 ? ? ? ? 89 7C 24 28 4C 8D 05 ? ? ? ? 33 D2 48 89 44 24 ? 33 C9 E8 ? ? ? ?").Get();
+        if (!RequestExitWithStatus)
+            RequestExitWithStatus = Memcury::Scanner::FindPattern("4C 8B DC 49 89 5B 08 49 89 6B 10 49 89 73 18 49 89 7B 20 41 56 48 83 EC 30 80 3D ? ? ? ? ? 49 8B").Get();
+        if (!RequestExitWithStatus)
+            RequestExitWithStatus = Memcury::Scanner::FindPattern("48 8B C4 48 89 58 18 88 50 10 88 48 08 57 48 83 EC 30").Get();
+
+        NullFuncs.push_back(RequestExitWithStatus);
     }
 
     if (VersionInfo.EngineVersion >= 4.21)
