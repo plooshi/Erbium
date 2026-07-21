ab1c6864a6c486dc8b5e1d0eadb4ba9d3c337dd7
Sarah <me@ploosh.dev>
2026-07-20 23:14:10 -0400

    codegen

--- Erbium/Erbium/Private/Finders.cpp
+++ Erbium/Erbium/Private/Finders.cpp
@@ -3250,6 +3250,10 @@ uint64 FindSelectAndSetupMyBuildingLevel()
 
 uint64 FindStreamInMyBuilding()
 {
+    if (VersionInfo.EngineVersion == 4.27)
+        return Memcury::Scanner::FindPattern(
+            "48 8B C4 48 89 58 ? 48 89 70 ? 48 89 78 ? 55 41 54 41 55 41 56 41 57 48 8D 6C 24 ? 48 81 EC ? ? ? ? 0F 29 70 ? 0F 29 78 ? 44 0F 29 40 ? 48 8B 05 ? ? ? ? 48 33 C4 48 89 45 ? 45 33 F6").Get();
+
     auto sRef = Memcury::Scanner::FindStringRef(L"%s.%s trying to load invalid level %s", false, 0, VersionInfo.FortniteVersion >= 17, false);
 
     if (!sRef.IsValid())
